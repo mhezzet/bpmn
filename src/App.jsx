70460d931d7600df6be5dbd67ce3de2a15f1db45
Mahmoud Ezzat <mhezzet@gmail.com>
2022-02-06 19:39:25 +0200

    data table

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
-import React, { useState } from 'react'
-import { Bpmn } from './Bpmn'
-import { BpmnSelector } from './BpmnSelector'
-import converter from 'xml-js'
+import React from 'react'
+import { Table } from './table'
+
+const rowData = [
+  { make: 'Toyota', model: 'Celica', price: 35000 },
+  { make: 'Ford', model: 'Mondeo', price: 32000 },
+  { make: 'Porsche', model: 'Boxter', price: 72000 },
+  { make: 'Toyota', model: 'Celica', price: 35000 },
+  { make: 'Ford', model: 'Mondeo', price: 32000 },
+  { make: 'Porsche', model: 'Boxter', price: 72000 },
+  { make: 'Toyota', model: 'Celica', price: 35000 },
+  { make: 'Ford', model: 'Mondeo', price: 32000 },
+  { make: 'Porsche', model: 'Boxter', price: 72000 },
+  { make: 'Toyota', model: 'Celica', price: 35000 },
+  { make: 'Ford', model: 'Mondeo', price: 32000 },
+  { make: 'Porsche', model: 'Boxter', price: 72000 },
+  { make: 'Toyota', model: 'Celica', price: 35000 },
+  { make: 'Ford', model: 'Mondeo', price: 32000 },
+  { make: 'Porsche', model: 'Boxter', price: 72000 },
+  { make: 'Toyota', model: 'Celica', price: 35000 },
+  { make: 'Ford', model: 'Mondeo', price: 32000 },
+  { make: 'Porsche', model: 'Boxter', price: 72000 },
+  { make: 'Toyota', model: 'Celica', price: 35000 },
+  { make: 'Ford', model: 'Mondeo', price: 32000 },
+  { make: 'Porsche', model: 'Boxter', price: 72000 },
+  { make: 'Toyota', model: 'Celica', price: 35000 },
+  { make: 'Ford', model: 'Mondeo', price: 32000 },
+  { make: 'Porsche', model: 'Boxter', price: 72000 },
+]
+
+const columns = [
+  { field: 'make', filter: true },
+  { field: 'model', sortable: true, filter: true },
+  { field: 'price', sortable: true, filter: true },
+]
 
 export const App = () => {
-  const [bpmnXML, setBpmnXML] = useState(null)
-
   return (
-    <div style={{ width: '100%', height: '60vh' }}>
-      <Bpmn
-        xml={bpmnXML}
-        onChange={data => {
-          console.log('data', converter.xml2json(data, { compact: true, spaces: 2 }))
-        }}
-      />
-      <BpmnSelector onChange={setBpmnXML} />
+    <div>
+      <Table columns={columns} data={rowData} paginationPageSize={15} />
     </div>
   )
 }
